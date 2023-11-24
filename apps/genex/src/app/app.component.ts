5d445a702167fe07adb258229096e8865d334cfa
Jorge Gonçalves <jrcgonc@gmail.com>
2023-11-24 13:56:47 +0000

    Initial commit

---
+++ apps/genex/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'genex-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'genex';
+}
